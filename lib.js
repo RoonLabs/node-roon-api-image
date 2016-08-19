1d6f56f7c95662e34969b0b74d64abc6f92f24e6
Danny Dulai <danny@ishiboo.com>
2016-08-19 15:15:44 -0400

    importing initial files

---
+++ lib.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let SVCNAME = "com.roonlabs.image:1";
+
+function RoonApiImage(core) {
+    this.core = core;
+}
+
+RoonApiImage.services = [ { name: SVCNAME } ];
+
+RoonApiImage.prototype.get_image = function(image_key, opts, cb) {
+    opts = Object.assign({ image_key: image_key }, opts);
+    core.moo.send_request(SVCNAME+"/get_image",
+                          opts,
+                          (msg, body) => {
+                              if (msg && msg.name == "Success") cb(false, msg.content_type, body);
+                              else cb(msg ? msg.name : "NetworkError");
+                          });
+};
+
+exports = module.exports = RoonApiImage;
